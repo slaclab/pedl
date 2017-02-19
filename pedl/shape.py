de54d8b8c3e30c917cc984200b1e9c1ae1c11014
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-02-19 15:44:14 -0800

    Shape work coming into focus

--- pedl/shape.py
+++ pedl/shape.py
@@ -1,12 +1,35 @@
-from .widget import Widget
-
+from .widget  import Widget
+from .choices import ColorChoice 
 
 class Shape(Widget):
-    
-    def __init__(self, x=0, y=0, w=100, h=100, fill=None):
+
+    _linewidth = 1
+    _lineColor = ColorChoice.Black
+
+    @property
+    def linewidth(self):
+        return self._linewidth
+
+    @linewidth.setter
+    def linewidth(self, value):
+        return self._linewidth = self.set_property(value, dtype=int)
+
+    @property
+    def lineColor(self):
+        return self._lineColor
+
+    @lineColor.setter(self, value):
+        return self._lineColor = self.set_property(value, dtype=ColorChoice)
 
 
+class Circle(Shape):
+    widgetClass = 'activeCircleClass'
+    minor   = 0
+    release = 0
 
-class Circle
 
-class Rectangle
+class Rectangle(Shape):
+    widgetClass = 'activeRectangleClass'
+    minor   = 0
+    release = 0
+
