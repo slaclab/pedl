395c0103c59db70f35788703795f7f8b9ee1ff42
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-02-21 21:32:00 -0800

    Basic setuptools packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import (setup, find_packages)
+
+
+setup(name    = 'pedl',
+      version = '0.0.1',
+      license = 'BSD-like',
+      author  = 'SLAC National Accelerator Laboratory',
+
+      packages    = find_packages(),
+      description = 'Qt Inspired Wrapper for creation of EDM files'
+
+    )
