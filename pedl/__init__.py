393b371e059b2ff25d29643290e935d13e138075
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-02-28 17:27:01 -0800

    Installed versioneer

--- pedl/__init__.py
+++ pedl/__init__.py
@@ -5,3 +5,7 @@ from .widgets.shape  import Rectangle, Circle
 from .visibility     import Visibility
 from .designer       import Designer
 from .layout         import VBoxLayout, HBoxLayout, StackLayout
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
