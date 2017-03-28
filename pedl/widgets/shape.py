ae419df7403dd86280eb390cf46c446a36d3d371
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-03-28 16:37:50 -0700

    Modifed all shapes to use pedlproperties

--- pedl/widgets/shape.py
+++ pedl/widgets/shape.py
@@ -5,60 +5,17 @@ drawing command, we have a shared :class:`.Shape` object that each inherits
 """
 from ..widget  import Widget
 from ..choices import ColorChoice 
-
+from ..utils   import pedlproperty
 
 class Shape(Widget):
     """
     Basic Shape Widget
     """
     template   = 'shape.edl'
-    _linewidth = 1
-    _fill      = None
-    _lineColor = ColorChoice.Black
-
-    @property
-    def lineWidth(self):
-        """
-        Width of the frame around the Widget
-        """
-        return self._linewidth
-
-    @lineWidth.setter
-    def lineWidth(self, value):
-        self._linewidth = int(value) 
-
-    @property
-    def lineColor(self):
-        """
-        Color of the frame around the Widget expressed as a
-        :class:`.ColorChoice`
-        """
-        return self._lineColor
-
-    @lineColor.setter
-    def lineColor(self, value):
-        self._lineColor = ColorChoice(value)
-
-    @property
-    def fill(self):
-        """
-        Background fill in the Widget, if set to None, the display background
-        is used. Otherwise this should be a named color provided in the
-        :class:`.ColorChoice` or if the color you want is unavailable an
-        integer in ``(0,94)`` specifying the index of the desired color 
-        """
-        return self._fill
-
-
-    @fill.setter
-    def fill(self, value):
-        if value is None:
-            self._fill = value
-
-        else:
-            self._fill = ColorChoice(value)
-
-
+    linewidth = pedlproperty(int, default=1, doc='Stroke of surrounding border')
+    fill      = pedlproperty(ColorChoice, doc='Background fill in the widget')
+    lineColor = pedlproperty(ColorChoice, default=ColorChoice.Black,
+                             doc='Color of surrounding Widget frame')
 
 class Circle(Shape):
     widgetClass = 'activeCircleClass'
