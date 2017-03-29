5ff78592e284bb0846cc24618b5b58b627ec220a
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-03-29 10:24:58 -0700

    Changed for renaming of StackedLayout

--- pedl/__init__.py
+++ pedl/__init__.py
@@ -3,7 +3,7 @@ from .widget         import Widget
 from .widgets.shape  import Rectangle, Circle
 from .utils          import Font, Visibility, launch
 from .designer       import Designer
-from .layout         import VBoxLayout, HBoxLayout, StackLayout
+from .layout         import VBoxLayout, HBoxLayout, StackedLayout
 
 from ._version import get_versions
 __version__ = get_versions()['version']
