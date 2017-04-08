3e3695ef4de5e12311c3072e5a161c4401ea9282
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-04-08 09:53:26 -0700

    Explicit import of EmbeddedWindow

--- pedl/widgets/__init__.py
+++ pedl/widgets/__init__.py
@@ -1,3 +1,4 @@
-from .text   import StaticText
-from .shape  import Shape, Rectangle, Circle
-from .button import MenuButton, MessageButton
+from .text     import StaticText
+from .shape    import Shape, Rectangle, Circle
+from .button   import MenuButton, MessageButton
+from .embedded import EmbeddedWindow
