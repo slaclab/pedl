270d04f026ff7badc0165559c10b5971c893e5af
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-04-25 17:55:41 -0700

    API: Explicit import of new line widgets

--- pedl/widgets/__init__.py
+++ pedl/widgets/__init__.py
@@ -1,4 +1,4 @@
 from .text     import StaticText
-from .shape    import Shape, Rectangle, Circle
+from .shape    import Shape, GateValve, Stopper, Camera, Rectangle, Circle
 from .button   import MenuButton, MessageButton
 from .embedded import EmbeddedWindow
